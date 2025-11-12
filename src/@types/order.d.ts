91a9df40c0ebd0b9af2b2dc9388daf749bd8c58c
varel <varellcaesar@gmail.com>
2025-11-13 00:42:45 +0700

    integrate order-detail, order-activities

---
+++ src/@types/order.d.ts
@@ -0,0 +1,85 @@
+interface ListOrderResponseData {
+  list: ListOrder[];
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  nextPage: number;
+  page: number;
+  prevPage: number;
+  total: number;
+  totalPages: number;
+}
+
+interface ListOrder {
+  id: string;
+  resellerNumber: string;
+  orderNumber: string;
+  status: string;
+  periode: string;
+  amount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  createdBy: string;
+  updatedBy: string;
+  deletedBy: null;
+}
+
+interface ListOrderDetail {
+  id: string;
+  clientId: string;
+  orderId: string;
+  email: string;
+  clientName: string;
+  status: string;
+  phoneNumber: string;
+  package: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: null;
+  createdBy: string;
+  updatedBy: string;
+  deletedBy: null;
+}
+
+interface ListOrderDetailResponseData {
+  list: ListOrderDetail[];
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  nextPage: number;
+  page: number;
+  prevPage: number;
+  total: number;
+  totalPages: number;
+}
+
+interface OrderActivitiesResponseData {
+  id: string;
+  activityName: string;
+  status: string;
+  sequence: number;
+  orderDetailId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: null;
+  createdBy: string;
+  updatedBy: string;
+  deletedBy: null;
+}
+
+type ListOrderParam =
+  typeof import("@rtrw-monitoring-system/app/constants").PARAMS.orderListParam;
+type ListOrderFilter =
+  import("use-query-params").DecodedValueMap<ListOrderParam>;
+type ListOrderFilterPayload = Omit<ListOrderFilter, "page" | "limit">;
+
+type ListOrderDetailParam =
+  typeof import("@rtrw-monitoring-system/app/constants").PARAMS.orderDetailListParam;
+type ListOrderDetailFilter =
+  import("use-query-params").DecodedValueMap<ListOrderDetailParam>;
+type ListOrderDetailFilterPayload = Omit<
+  ListOrderDetailFilter,
+  "page" | "limit"
+>;
+
+type ListOrderResponse = BaseResponse<ListOrderResponseData>;
+type ListOrderDetailRespone = BaseResponse<ListOrderDetailResponseData>;
+type OrderActivitiesResponse = BaseResponse<OrderActivitiesResponseData[]>;
